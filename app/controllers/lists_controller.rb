b3b94d7d85664550e8f7a458a153aaf63899fbed
Irina Borisova <i.a.borisova@gmail.com>
2022-05-20 11:33:36 +0200

    Seed, some style, routs done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  movie = Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Address.street_address,
+    poster_url: Faker::Internet.url,
+    rating: rand(1..5)
+    # chef_name: ['Alain Ducasse', 'Pierre Gagnaire', 'Martin Berasategui', 'Yannick Alleno', 'Anne-Sophie Pic'].sample
+  )
+  puts "Created restaurant #{movie.title}"
+end
